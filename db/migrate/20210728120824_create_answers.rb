51b9186094e49558c24031052b683f2c5ed74229
Anna Shigirdanova <anna.shigirdanova@yandex.com>
2021-07-28 15:41:24 +0300

    add tests and model for answers

---
+++ db/migrate/20210728120824_create_answers.rb
@@ -0,0 +1,11 @@
+class CreateAnswers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :answers do |t|
+      t.string :title, presence: true
+      t.text :body
+      t.references :question, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
