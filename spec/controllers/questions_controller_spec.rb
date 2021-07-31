95bd74db724e1699edb03906c2ab4fb600965183
Anna Shigirdanova <anna.shigirdanova@yandex.com>
2021-07-31 17:14:20 +0300

    add tests for Question#new action

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -27,4 +27,18 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'assigns a new Question to @question' do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+
+
+  end
+
 end
