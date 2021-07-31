354136c16b1dea1ca26ae8a7b7dd61e087f6da88
Anna Shigirdanova <anna.shigirdanova@yandex.com>
2021-07-31 17:44:28 +0300

    refactor of simple tests

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,21 @@
 class QuestionsController < ApplicationController
+  before_action :load_question, only: %i[show edit]
+
   def index
     @questions = Question.all
   end
 
-  def show
-    @question = Question.find(params[:id])
-  end
+  def show; end
 
   def new
     @question = Question.new
   end
 
-  def edit
+  def edit; end
+
+  private
+
+  def load_question
     @question = Question.find(params[:id])
   end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,4 +1,6 @@
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
   describe 'GET #index' do
     let(:questions) { create_list(:question, 3) }
 
@@ -14,8 +16,6 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #show' do
-    let(:question) { create(:question) }
-
     before { get :show, params: { id: question }}
 
     it 'assigns the requested question to @question' do
@@ -40,9 +40,7 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #edit' do
-    let(:question) { create(:question) }
-
-    before { get :edit, params: { id: question }}
+    before { get :edit, params: { id: question } }
 
     it 'assigns the requested question to @question' do
       expect(assigns(:question)).to eq question
