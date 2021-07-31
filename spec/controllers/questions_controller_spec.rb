f0dc3c59e23a9a99a038d758fb0de446af042c8a
Anna Shigirdanova <anna.shigirdanova@yandex.com>
2021-07-31 18:57:24 +0300

    add tests for Question#destroy action

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -112,5 +112,17 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
+  describe 'DELETE #destroy' do
+    let!(:question) { create(:question) }
+
+    it 'deletes the question' do
+      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
+    end
+
+    it 'redirect to index' do
+      delete :destroy, params: { id: question }
+      expect(response).to redirect_to question_path
+    end
+  end
 
 end
