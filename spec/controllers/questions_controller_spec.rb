94784967b79240a447a8ae968ceef7b89824d34c
Anna Shigirdanova <anna.shigirdanova@yandex.com>
2021-07-31 17:09:16 +0300

    add tests for Question#show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     let(:questions) { create_list(:question, 3) }
 
-    before {get :index}
+    before { get :index }
 
     it 'populates an array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
@@ -10,6 +10,20 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'render index view' do
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+
+    before { get :show, params: { id: question }}
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
     end
   end
 
