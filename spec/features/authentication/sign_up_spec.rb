9b9adfdcdecbd7306cfe9b1f3274d7d484711748
Anna Shigirdanova <anna.shigirdanova@yandex.com>
2021-08-12 20:38:05 +0300

    rename files

---
+++ spec/features/authentication/sign_up_spec.rb
@@ -0,0 +1,19 @@
+feature 'User can sign up', "
+  In order to use all the features
+  As an unregistered user
+  I'd like to be able to sign up
+" do
+
+  given(:user) { create(:user) }
+
+  background { visit new_user_registration_path }
+
+  scenario 'Unregistered user tries to sign up' do
+    fill_in 'Email', with: 'unregistered_user@test.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+end
